5a89a2e630afc1e931df0807584a2af94d1af94a
MBATHE <mbathepaul@gmail.com>
2026-02-20 09:45:08 +0100

    chore: ignore .docusaurus generated files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/home/paul/codes/smart_eneo_server-main/eyeflow/node_modules/infima/dist/css/default/default.css'),
-  require('/home/paul/codes/smart_eneo_server-main/eyeflow/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/paul/codes/smart_eneo_server-main/eyeflow/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/paul/codes/smart_eneo_server-main/eyeflow/src/css/custom.css'),
-];
